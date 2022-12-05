8360ce76477023deea1a24b92d4ebcac77cca455
Orestis Makris <79021400+OrestisMakris@users.noreply.github.com>
2022-12-05 02:25:27 +0200

    Orestis Inserts to tables branch , phones , workers (admins)
    Ver_1

---
+++ Insert Orestis.sql
@@ -0,0 +1,34 @@
+INSERT INTO branch VALUES
+(null , 'Kifisias ', 355 , 'Athens'),
+(null , 'Amarisias Artemidos' , 5 , 'Athens'),
+(null , 'Patra', 44 , 'Mezonos'),
+(null , 'Kalamata' , 5 , 'Aristomenous'),
+(null , 'Thesalonikh' , 55 , 'Tsimiskh');
+
+INSERT INTO phones VALUES
+(1 , '2101111111'),
+(1 ,  '2101222222'),
+(2 , '2102222222'),
+(2 , '2102333333'),
+(3 , '2103333333'),
+(4 ,  '2104444444'),
+(5 ,  '2105555555');
+
+INSERT INTO worker VALUES
+('AK10311111' , 'Giannhs' , 'Vasiliou'),
+('AK10322222' , 'Artemis' ,'Papadopoulou'),
+('AK10333333' ,  'Georgia' ,'Makri'),
+('AK10344444' ,  'Greg' ,'Stephanopoulos'),
+('AK10355555' ,  'Iason', 'Raikos'),
+('AK10366666' ,  'Maria', 'Papadopoulou'),
+('AK10377777' ,  'Orestis', 'Makris'),
+('AK10388888' ,  'Damianos' , 'Papadopoulos'),
+('AK10399999' , 'Iris' , ''),
+('AK10310000' ,  'John'),
+('AK10310111' ,  'Alkis'),
+('AK10310222' ,  'Andreas'),
+('AK10310333' ,  'Giannhs'),
+('AK10310444' ,  'Sofia'),
+('AK10310555' , 'Nikos');
+
+
