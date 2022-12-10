160e0eaade9b9f5d2768ed1b3871c1b1f649f6d3
gregdeli <gregorydel@hotmail.gr>
2022-12-10 23:31:15 +0200

    a

---
+++ insert_drv,guid,lang.sql
@@ -0,0 +1,34 @@
+insert into driver values 
+('AM10410000','D','LOCAL',9),
+('AM10410111','D','ABROAD',10),
+('AM10410222','D','LOCAL',7),
+('AM10410333','D','ABROAD',11),
+('AM10410444','D','LOCAL',20),
+('AM10410555','D','LOCAL',15),
+('AM10410666','D','LOCAL',8),
+('AM10410777','B','ABROAD',3);
+
+insert into guide values 
+('AK10311111', 'Employment History:\n\t\t1997 - 2000 Accountant at Logistiko Grafeio "H Lysi"
+\n\t\t2000 - 2010 Travel Guide at "Joy Tours"'),
+('AN10520010', 'Employment History:\n\t\t1999 - 2005 Travel Guide at "Pimenidis Travel"'),
+('AN10520020', 'Employment History:\n\t\t1995 - 2002 Travel Guide at "TEMPI HOLIDAY"
+\n\t\t2002 - 2011 Travel Guide at "KAVVADIAS TOURS"
+\n\t\t2011 - 2017 Travel Guide at "ART TOURS"'),
+('AN10520030','Employment History:\n\t\t2005 - 2016 Travel Guide at "DIOTOURS N.G & RED ELEPHANT"'),
+('AN10520040','Employment History:\n\t\t2001 - 2019 Travel Guide at "Rania TOURS"'),
+('AN10520050','Employment History:\n\t\t1990 - 2002 Travel Guide at "Penguin Travel Tours"'),
+('AK10310444','Employment History:\n\t\t1999 - 2006 Accountant at "PANIDIS PAYLOS LOGISTIKO GRAFEIO"
+\n\t\t2010 - 2018 Travel Guide at "Herodotus TOURS"'),
+('AN10520060','Employment History:\n\t\t1989 - 1999 Travel Guide at "Mazi Travel & Events"
+\n\t\t2000 - 2010 Travel Guide at "Prima Holidays"');
+
+insert into languages values 
+('AK10311111','Greek, English, French'),
+('AN10520010','Greek, English, German'),
+('AN10520020','Greek, English, German'),
+('AN10520030','Greek, English, French, Italian'),
+('AN10520040','Greek, English, Italian'),
+('AN10520050','Greek, English, German, Italian'),
+('AK10310444','Greek, English'),
+('AN10520060','Greek, English, French, Italian');
