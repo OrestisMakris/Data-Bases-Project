5770b7e77c9a345cc972f39bf82123fcf0f49d01
Orestis Makris <79021400+OrestisMakris@users.noreply.github.com>
2022-12-21 17:00:18 +0200

    ;p

--- Lab ASK5 Inserts 1.sql
+++ Lab ASK5 Inserts 1.sql
@@ -1,10 +1,9 @@
 -- Active: 1670782097134@@127.0.0.1@3306@travel_agency
 
-INSERT INTO trip VALUES
-(null, '2023-08-19 18:30:00', '2024-08-27 10:30:00', 2, 1000, 5, 'AN10520060', 'AM10410777');
+#INSERT INTO trip VALUES
+#(null, '2023-08-19 18:30:00', '2024-08-27 10:30:00', 2, 1000, 5, 'AN10520060', 'AM10410777');
 
 
 INSERT INTO reservation VALUES
 (31, 1, 'Andreas', 'Tate', 'ADULT'),
-(31, 2, 'Xara', 'Papaderou', 'ADULT'),
-(31, 3, 'Konstantinos', 'Papaderos', 'MINOR');
+(31, 2, 'Xara', 'Papaderou', 'ADULT');
---
+++ create-db-template.sql
@@ -0,0 +1,26 @@
+-- Active: 1670782097134@@127.0.0.1@3306
+>DELIMITER $ 
+DROP PROCEDURE IF EXISTS check_reservations_trip$
+CREATE PROCEDURE check_reservations_trip(IN trip_id INT , OUT flag INT) 
+BEGIN
+DECLARE max_seats INT;
+DECLARE same_res_tr_id INT;
+
+SET same_res_tr_id=0;
+SET max_seats=0;
+
+SELECT COUNT(*) INTO same_res_tr_id FROM reservation INNER JOIN trip ON reservation.res_tr_id = trip.tr_id WHERE trip_id = res_tr_id;
+
+SELECT tr_maxseats INTO max_seats FROM trip WHERE trip_id = tr_id;
+
+IF (max_seats > same_res_tr_id)
+THEN
+SET flag=1;
+END IF;
+
+END$
+DELIMITER ;
+
+CALL check_reservations_trip(31,@flag);
+
+SELECT @flag;
