82064e5acc504f72568419fda03d79b386fb575d
gregdeli <gregorydel@hotmail.gr>
2023-01-10 14:27:02 +0200

    a

--- mysqlr_greg_testing.py
+++ mysqlr_greg_testing.py
@@ -4,7 +4,7 @@ import csv
 from io import StringIO
 nam =[]
 last =[]
-with open('C:\\Users\\Orestis\\Downloads\\names.txt', 'r' ,encoding= 'utf-16') as names:                                                                                        
+with open('names.txt', 'r' ,encoding= 'utf-16') as names:                                                                                        
  for line in names:
         row =line.split()
         nam.append(row[:-1])
@@ -15,7 +15,7 @@ with open('C:\\Users\\Orestis\\Downloads\\names.txt', 'r' ,encoding= 'utf-16') a
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="",
+  password="giagia12",
   database="travel_agency"
 )
 
@@ -26,9 +26,9 @@ for i in range(1,65001,1):
  Name = nam[index][0]
  lastname = last[index]
  amount = random.randint(70, 200)
- id = random.randint(1, 3)
+ offer_id = random.randint(1, 3)
  sql = "INSERT INTO reservation_offers (rsv_name, rsv_lastname , deposit_amount , rsv_offer_id) VALUES (%s, %s ,%s, %s)"
- val = ( Name,lastname ,amount ,id)
+ val = (Name,lastname,amount,offer_id)
  mycursor.execute(sql, val)
  mydb.commit()
 print(mycursor.rowcount, "record inserted.")
