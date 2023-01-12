7e3d10949f12897f144778d392b963b0a97dad61
gregdeli <gregorydel@hotmail.gr>
2023-01-12 19:00:50 +0200

    bjh

--- greg/procedures.sql
+++ greg/procedures.sql
@@ -19,64 +19,52 @@ DROP PROCEDURE IF EXISTS res_names;
 DELIMITER $
 CREATE PROCEDURE res_names(IN lname VARCHAR(20))
 BEGIN
-    DECLARE fistname VARCHAR(20);
-    DECLARE lastname VARCHAR(20);
+    DECLARE flag INT; 
     DECLARE offer_id INT;
     DECLARE no_of_res INT;
     DECLARE finishedFlag INT;
-    DECLARE resCurs CURSOR FOR
+    DECLARE multResCurs CURSOR FOR
     SELECT rsv_offer_id, COUNT(rsv_offer_id)
     FROM reservation_offers
     WHERE rsv_lastname=lname
-    GROUP BY rsv_offer_id;
+    GROUP BY rsv_offer_id
+    HAVING COUNT(rsv_offer_id)>1;
+    DECLARE singleResCurs CURSOR FOR
+    SELECT rsv_offer_id, COUNT(rsv_offer_id)
+    FROM reservation_offers
+    WHERE rsv_lastname=lname
+    GROUP BY rsv_offer_id
+    HAVING COUNT(rsv_offer_id)=1;
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET finishedFlag=1;
-    OPEN resCurs;
+    OPEN multResCurs;
     SET finishedFlag=0;
-    FETCH resCurs INTO offer_id, no_of_res;
+    FETCH multResCurs INTO offer_id, no_of_res;
+    SET flag = 0;
     WHILE(finishedFlag=0) DO
-        IF(no_of_res>1) THEN
+        IF(flag = 0) THEN
             SELECT 'Reserved offers for last name' AS '', lname AS '';
-            SELECT offer_id AS 'Offer Id', no_of_res AS 'Number of reserved offers';
         END IF;
-        FETCH resCurs INTO offer_id,no_of_res; 
+        SELECT offer_id AS 'Offer Id', no_of_res AS 'Number of reserved offers';
+        FETCH multResCurs INTO offer_id,no_of_res; 
+        SET flag =  1;
     END WHILE;
-    CLOSE resCurs;
-    OPEN resCurs;
-    SET finishedFlag = 0;
-    FETCH resCurs INTO offer_id, no_of_res;
+    CLOSE multResCurs;
+    OPEN singleResCurs;
+    SET finishedFlag=0;
+    SET flag = 0;
+    FETCH singleResCurs INTO offer_id, no_of_res;
     WHILE(finishedFlag=0) DO
-        IF(no_of_res=1) THEN
+        IF(flag = 0) THEN
             SELECT 'Reserved offers with one reservation per offer' AS '';
-            SELECT t1.rsv_name, t1.rsv_lastname, t2.rsv_offer_id
-            FROM 
-            (SELECT rsv_name, rsv_lastname, rsv_offer_id
-            FROM reservation_offers
-            WHERE rsv_lastname=lname) AS t1
-            INNER JOIN 
-            (SELECT rsv_offer_id, COUNT(rsv_offer_id)
-            FROM reservation_offers
-            WHERE rsv_lastname=lname
-            GROUP BY rsv_offer_id
-            HAVING COUNT(rsv_offer_id)=1) AS t2
-            ON t1.rsv_offer_id = t2.rsv_offer_id;
-        
         END IF;
-        FETCH resCurs INTO offer_id,no_of_res;
+        SELECT rsv_name, rsv_lastname, rsv_offer_id
+        FROM reservation_offers
+        WHERE rsv_offer_id = offer_id AND rsv_lastname = lname;
+        SET flag = 1;
+        FETCH singleResCurs INTO offer_id,no_of_res;
     END WHILE;
 END$
 DELIMITER ;
-CALL res_names('Deli');
+CALL res_names('deli');
 
---without count
-SELECT rsv_name, rsv_lastname, rsv_offer_id
-FROM reservation_offers
-WHERE rsv_lastname='Deli'
-ORDER BY rsv_offer_id;
-
---with count
-SELECT 'Reserved offers for last name' AS '', lname AS '';
-    SELECT rsv_offer_id AS 'Offer Id',COUNT(rsv_offer_id) AS 'Number of reserved offers', rsv_lastname
-    FROM reservation_offers
-    WHERE rsv_lastname='Deli'
-    GROUP BY rsv_offer_id
-    HAVING COUNT(rsv_offer_id)=1;
+--insert deli 
