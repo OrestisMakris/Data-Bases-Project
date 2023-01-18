e48d25233445f484eac88053262e899c0643d01a
gregdeli <gregorydel@hotmail.gr>
2023-01-18 14:59:16 +0200

    a

---
+++ greg/Data_Bases_Project/JavaApplication1/src/LogIN_SWI/login.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LogIN_SWI;
+
+/**
+ *
+ * @author Orestis
+ */
+public class login {
+    
+    public static void main(String[] args){
+        log_in log = new log_in();
+        log.show();
+    }
+    
+}
