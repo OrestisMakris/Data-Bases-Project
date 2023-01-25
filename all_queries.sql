3ac7a401107f2436dc6d9d80423c5bcc8df7c165
Kaffeinrider <iasonasraikos@gmail.com>
2023-01-26 00:57:43 +0200

    Trigger change

--- all_queries.sql
+++ all_queries.sql
@@ -1136,6 +1136,19 @@ BEGIN
 END$
 DELIMITER ;
 
+/*trigger 3.1.4.2*/
+DROP TRIGGER IF EXISTS check_reservations;
+DELIMITER $
+CREATE TRIGGER check_reservations BEFORE UPDATE ON trip
+FOR EACH ROW
+BEGIN
+IF exists(SELECT * FROM reservation WHERE res_tr_id=OLD.tr_id)
+THEN
+SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT='This trip already has reservations therefore updates are restricted!';
+END IF;
+END$
+DELIMITER ;
+
 /*trigger 3.1.4.3*/
 DROP TRIGGER IF EXISTS check_salary;
 DELIMITER $
